94570fa24fea3ee45d9ac035ddff5b7e69677528
Quinten <quintenpalmer@gmail.com>
2013-07-26 20:06:00 +0000

    Rendering a template for home page

--- webserver/home/views.py
+++ webserver/home/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return render_to_response("play.html")
