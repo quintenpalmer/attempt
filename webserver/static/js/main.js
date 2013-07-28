ddd7c425eab6d29051ee50c47ed0d63273062c06
Quinten <quintenpalmer@gmail.com>
2013-07-28 19:16:24 -0400

    Update HTML/JS to use websockets correctly

--- webserver/static/js/main.js
+++ webserver/static/js/main.js
@@ -1,12 +1,18 @@
 console.log("Hello Console!");
 host = window.location.host.split(":")[0];
 console.log(host);
-conn = new WebSocket("ws://"+host+":8888/ws");
-conn.onclose = function(evt) {
-	console.log("connection closed");
-}
-conn.onmessage = function(evt) {
-	console.log("got a message");
-}
+var conn;
+$(document).ready(function () {
+    conn = new WebSocket("ws://"+host+":8888/ws");
+    conn.onclose = function(evt) {
+        console.log("connection closed");
+    }
+    conn.onmessage = function(evt) {
+        console.log("got a message");
+    }
 
-conn.send("hi");
+});
+
+function sendit() {
+    console.log(conn.send("hi"));
+}
