098aadfbe57acaf2a3fec042235497d069245016
Quinten <quintenpalmer@gmail.com>
2013-07-28 18:44:15 -0400

    Javascript code to make a websocket call

--- webserver/static/js/main.js
+++ webserver/static/js/main.js
@@ -1,1 +1,12 @@
 console.log("Hello Console!");
+host = window.location.host.split(":")[0];
+console.log(host);
+conn = new WebSocket("ws://"+host+":8888/ws");
+conn.onclose = function(evt) {
+	console.log("connection closed");
+}
+conn.onmessage = function(evt) {
+	console.log("got a message");
+}
+
+conn.send("hi");
