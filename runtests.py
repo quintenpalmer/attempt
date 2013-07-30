8058d2cbd8bbe7d9f9df8f337f020a21692367a7
Kyle Phelps <kylep91@gmail.com>
2013-07-29 21:37:51 -0400

    Ignore logging library

--- runtests.py
+++ runtests.py
@@ -5,6 +5,7 @@ import os
 def main():
     packages = os.listdir(os.path.join(os.getcwd(), "src"))
     packages.remove("github.com")
+    packages.remove("cgl.tideland.biz")
     os.system("go test " + " ".join(packages))
 
 if __name__ == '__main__':
